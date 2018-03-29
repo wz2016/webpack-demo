9356d0e78e47f3d1c1c959f082ad455cadd0e54c
Thomas <weiluzhao@paypal.com>
2018-03-29 16:22:00 -0500

    Update Hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// using webpack to generate html home page and creat app.bundle.js for business logic
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css';
 
 function component() {
   var element = document.createElement('div');
@@ -16,4 +18,18 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    // printMe();
+
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  })
+}
--- src/print.js
+++ src/print.js
@@ -1,3 +1,3 @@
 export default function printMe() {
-  console.log('I love weiran!!!');
+  console.log('I love weiran!!! ....');
 }
