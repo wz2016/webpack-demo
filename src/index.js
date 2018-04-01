56ed61af4f9c9e326166f68baf5300922e29e69f
Thomas <weiluzhao@paypal.com>
2018-04-01 16:37:11 -0500

    Dynamic import code splitting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
-function component() {
+async function getComponent() {
   var element = document.createElement('div');
-  var btn = document.createElement('button');
+  // var btn = document.createElement('button');
+  //
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
+  //
+  //
+  // btn.innerHTML = 'Click me';
+  // btn.onclick = printMe;
+  //
+  // element.appendChild(btn);
 
+  // const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-
-  btn.innerHTML = 'Click me';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+//
+// getComponent().then(commponent => {
+//   document.body.appendChild(commponent);
+// })
+//
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'output management'
+      title: 'Code Splitting'
     })
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common' // Specify the common bundle's name.
+    // })
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
