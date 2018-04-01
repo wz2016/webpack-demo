8c1c765d567650cc92dd32168dcbe3867037128f
Thomas <weiluzhao@paypal.com>
2018-04-01 16:13:51 -0500

    Production demo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
